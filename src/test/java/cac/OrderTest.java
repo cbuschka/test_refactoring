c0a2850bafcd5a230b85de0913559fec1b0f32cf
conni <cbuschka@gmail.com>
2023-11-16 12:38:58 +0100

    Added refactored test.

--- src/test/java/cac/OrderTest.java
+++ src/test/java/cac/OrderTest.java
@@ -17,31 +17,55 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class OrderTest {
 
+  private static final String NEXT_RECEIPT = "C1";
+  private static final String ORDER_NO = "O1";
+  private static final OffsetDateTime CANCELLED_AT = OffsetDateTime.now();
   @Mock
   private OrderRepository orderRepository;
   @Mock
   private ReceiptGenerator receiptGenerator;
-  @InjectMocks
-  private CancelOrderUseCase cancelOrderUseCase;
   @Captor
   private ArgumentCaptor<Order> orderCaptor;
+  @InjectMocks
+  private CancelOrderUseCase cancelOrderUseCase;
+  private Order givenOrder;
+  private String resultingCancellationReceipt;
 
   @Test
   void shouldCancelOrder() {
-    // given is an order not cancelled yet
-    Order order = new Order("O1", OffsetDateTime.now(), null, null);
-    when(orderRepository.findByOrderNo("O1")).thenReturn(Optional.of(order));
-    String nextReceipt = "C1";
-    when(receiptGenerator.nextReceipt()).thenReturn(nextReceipt);
+    givenIsAnOrderNotCancelledYet();
+    givenIsAnreceiptForCancellation();
 
-    // when the order is cancelled
-    OffsetDateTime cancelledAt = OffsetDateTime.now();
-    String cancellationReceipt = cancelOrderUseCase.cancel("O1", cancelledAt);
+    whenCancellationIsRequested();
 
-    // then it should be in cancelled state and save
-    assertThat(cancellationReceipt).isEqualTo(nextReceipt);
-    verify(orderRepository).save(orderCaptor.capture());
+    thenTheCancellationReceiptIsReturned();
+    thenTheOrderWasSaved();
+    thenTheSavedOrderWasCancelled();
+  }
+
+  private void thenTheCancellationReceiptIsReturned() {
+    assertThat(resultingCancellationReceipt).isEqualTo(NEXT_RECEIPT);
+  }
+
+  private void thenTheSavedOrderWasCancelled() {
     Order orderSaved = orderCaptor.getValue();
-    assertThat(orderSaved.cancelledAt()).isEqualTo(cancelledAt);
+    assertThat(orderSaved.cancelledAt()).isEqualTo(CANCELLED_AT);
+  }
+
+  private void thenTheOrderWasSaved() {
+    verify(orderRepository).save(orderCaptor.capture());
+  }
+
+  private void whenCancellationIsRequested() {
+    resultingCancellationReceipt = cancelOrderUseCase.cancel(ORDER_NO, CANCELLED_AT);
+  }
+
+  private void givenIsAnreceiptForCancellation() {
+    when(receiptGenerator.nextReceipt()).thenReturn(NEXT_RECEIPT);
+  }
+
+  private void givenIsAnOrderNotCancelledYet() {
+    givenOrder = new Order(ORDER_NO, OffsetDateTime.now(), null, null);
+    when(orderRepository.findByOrderNo(ORDER_NO)).thenReturn(Optional.of(givenOrder));
   }
 }
